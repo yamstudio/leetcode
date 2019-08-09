02cb910c5ff880c5726f1ea2cdac3257547d27a2
Yujun Qin <yamstudio@users.noreply.github.com>
2019-08-08 18:39:28 -0700

    Update @ 20190808

---
+++ csharp/206.reverse-linked-list.cs
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=206 lang=csharp
+ *
+ * [206] Reverse Linked List
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode tmp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+        return prev;
+    }
+}
+
