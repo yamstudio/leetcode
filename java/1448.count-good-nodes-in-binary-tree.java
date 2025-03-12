6615aa6b29e4138e6d8555e37f15fa086cea703e
Yujun Qin <yamstudio@users.noreply.github.com>
2025-03-12 17:38:53 -0400

    Update @ 20250312 (#550)

---
+++ java/1448.count-good-nodes-in-binary-tree.java
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=1448 lang=java
+ *
+ * [1448] Count Good Nodes in Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int goodNodes(TreeNode root) {
+        return goodNodes(root, Integer.MIN_VALUE);
+    }
+
+    private static int goodNodes(TreeNode root, int max) {
+        if (root == null) {
+            return 0;
+        }
+        int ret = 0;
+        if (root.val >= max) {
+            ++ret;
+            max = root.val;
+        }
+        return ret + goodNodes(root.left, max) + goodNodes(root.right, max);
+    }
+}
+// @lc code=end
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
